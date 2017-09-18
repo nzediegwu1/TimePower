d547b16f8a885c0c062d455c31ac715e7ec1f557
nzediegwu1 <nzediegwu1@outlook.com>
2017-09-18 11:26:57 +0100

    UPDATED PACKAGE.JSON included changes that was mistakenly excluded in the last commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
-﻿var express = require('express');
-var router = express.Router();
+﻿const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.send("Welcome to TimePower");
+router.get('/', (req, res) => {
+    const user = {
+        name: 'King',
+        position: 'Universe',
+    };
+    res.send(`The Wisdom of God, the ${user.name} of the ${user.position} Rules the World!`);
 });
-//great
 module.exports = router;
